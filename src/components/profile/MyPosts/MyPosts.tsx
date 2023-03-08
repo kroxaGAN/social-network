96f9f0042b0233f6e0e5bd9e895155e25996a838
Andrei Hutsau <mwplt@mail.ru>
2023-03-09 00:17:31 +0300

    props to components

--- src/components/profile/MyPosts/MyPosts.tsx
+++ src/components/profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import s from "./MyPosts.module.css"
 import {Post} from "./Post/Post";
 
+
+
 export const MyPosts = () => {
     return <div className={s.content}>
         <div>
@@ -10,9 +12,9 @@ export const MyPosts = () => {
                 <textarea>!!!</textarea>
                 <button>Add new post</button>
             </div>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post message={"Hello my friend"} countLikes={15}/>
+            <Post message={"It's first commit"} countLikes={20}/>
+            <Post message={"Yeee"} countLikes={1}/>
         </div>
     </div>
 }
--- src/components/profile/MyPosts/Post/Post.tsx
+++ src/components/profile/MyPosts/Post/Post.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import s from "./Post.module.css"
 
-export const Post = () => {
+type PostPropsType={
+    message:string
+    countLikes:number
+}
+
+export const Post = (props:PostPropsType) => {
     return <div className={s.content}>
         <div>
             <div>
 
                 <div className={s.item}>
                     <img src={'https://trikky.ru/wp-content/blogs.dir/1/files/2020/08/17/2859972401.jpg'} alt={"ava"}/>
-                    post1
+                    {props.message}
                     <div>
-                        <span>like</span>
+                        <span>{props.countLikes}</span>
                     </div>
                 </div>
             </div>
