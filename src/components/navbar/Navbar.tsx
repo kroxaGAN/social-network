cd30a9130f9fe16bc36b09703de53c759c6e2590
Andrei Hutsau <mwplt@mail.ru>
2023-03-09 01:06:14 +0300

    route v5 NavLink and some css for active

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import s from "./Navbar.module.css"
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-    const classForItem=`${s.item} ${s.active}`
-    return <nav className={s.nav}>
-        <div className={classForItem}>
-            <a href={'/profile'}>Profile</a>
-        </div>
-        <div className={classForItem}>
-            <a href={'/dialogs'}>Messages</a>
-        </div>
-        <div className={classForItem}>
-            <a>News</a>
-        </div>
-        <div className={classForItem}>
-            <a>Music</a>
-        </div>
-        <div className={classForItem}>
-            <a>Settings</a>
-        </div>
-    </nav>
+    const classForItem = `${s.item} ${s.active}`
+    return (
+        <nav className={s.nav}>
+            <div className={classForItem}>
+                <NavLink to={'/profile'} activeClassName={s.active}>Profile</NavLink>
+            </div>
+            <div className={classForItem}>
+                <NavLink to={'/dialogs'} activeClassName={s.active}>Messages</NavLink>
+            </div>
+            <div className={classForItem}>
+                <NavLink to={'/news'} activeClassName={s.active}>News</NavLink>
+            </div>
+            <div className={classForItem}>
+                <NavLink to={'/music'} activeClassName={s.active}>Music</NavLink>
+            </div>
+            <div className={classForItem}>
+                <NavLink to={'/settings'} activeClassName={s.active}>Settings</NavLink>
+            </div>
+        </nav>
+    )
 }
