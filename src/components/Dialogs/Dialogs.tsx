046de2d6c5c8d9fc9deabaa839dd64412df7f3d5
Andrei Hutsau <mwplt@mail.ru>
2023-03-10 22:38:04 +0300

    made Dialogs css and some div

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,8 +2,22 @@ import s from './Dialogs.module.css'
 
 export const Dialogs=()=>{
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <h3>Dialogs</h3>
+                <div className={`${s.dialog} ${s.active}`}>Dima</div>
+                <div className={s.dialog}>Andreu</div>
+                <div className={s.dialog}>Dina</div>
+                <div className={s.dialog}>Valeiy</div>
+            </div>
+            <div>
+               <h3>Messages</h3>
+                <div className={s.messages}>
+                    <div className={s.message}>Hi</div>
+                    <div className={s.message}>it good</div>
+                    <div className={s.message}>wery good</div>
+                </div>
+            </div>
         </div>
     )
 }
